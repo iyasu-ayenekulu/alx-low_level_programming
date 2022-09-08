7cfb9dc8eeee82bc64782fc6e286fa88ab14fd32
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-08 15:24:15 -0700

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-/**
+#include <stdio.h>                                                                                                                                                             
+/**   
  * main-print the size of  various types
  *
- * Description:prints the size of various types on thecomputer it is compiled
+ * Description:prints the size of various types on thecomputer it is compiled                                                                                                  
  *
- * and run on
+ * and run on                                                                                                                                                                  
  *
- * Return:Always 0 (Success)
- */
-int main(void)
-{
-printf("size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %d byte(s)\n", sizeof(long int));
-printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("size of a float: %d byte(s)\n", sizeof(float));
+ * Return:Always 0 (Success)                                                                                                                                                   
+ */                                                                                                                                                                            
+int main(void)                                                                                                                                                                 
+{                                                                                                                                                                              
+printf("size of a char: %d byte(s)\n", sizeof(char));                                                                                                                          
+printf("size of an int: %d byte(s)\n", sizeof(int));                                                                                                                           
+printf("size of a long int: %d byte(s)\n", sizeof(long int));                                                                                                                  
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));                                                                                                        
+printf("size of a float: %d byte(s)\n", sizeof(float));                                                                                                                        
 return (0);
 }
