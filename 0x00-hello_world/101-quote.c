121738e63daf3444c377a23f2445e63b6bd929de
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-08 14:55:24 -0700

    code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
-{fprintf("and that piece of art is useful\n" - Dora Korpar, 2015-10-19");
-return (1);
+{
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ return (1);
 }
