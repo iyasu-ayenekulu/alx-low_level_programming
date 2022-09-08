73b5adcdee6f4cd37e797fd94de1b5bc4752149d
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-08 14:14:38 -0700

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 /**
- * main - print the string in the put  function
+ * main - print the size of  various types
  *
- * Description using the main function
- * this program prints "programming is like multilingual puzzle
- * Return 0
+ * Description:prints the size of various types on thecomputer it is compiled
+ * and run on 
+ *
+ * Return:Always 0(Success)
  */
 int main(void)
 {
 int a;
-log int b
-long long  int c;
-char d;
-float f;
-printf("size of a char:%1d byte(s)\n", sizeof(d));
-printf("size of an int:%1d byte(s)\n", sizeof(a));
-printf("size of a long int:%1d byte(s)\n", sizeof(b));
-printf("size of a long long int:%1d byte(s)\n", sizeof(c));
-printf("size of a float:%1d byte(s)\n", sizeof(f));
+printf("size of a char:%d byte(s)\n",(int)sizeof(char));
+printf("size of an int:%d byte(s)\n",(int)sizeof(int));
+printf("size of a long int:%d byte(s)\n",(int)sizeof(long int));
+printf("size of a long long int:%d byte(s)\n",(int)sizeof(long long int));
+printf("size of a float:%d byte(s)\n",(int)sizeof(float));
 return (0);
 }
