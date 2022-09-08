6413564e1e6a4e0d48dde82c82eb04e100636708
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-08 14:30:31 -0700

    code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,7 @@
  * Return:Always 0 (Success)
  */
 int main(void)
-{
-printf("size of a char:%d byte(s)\n", (int)sizeof(char));
+{printf("size of a char:%d byte(s)\n", (int)sizeof(char));
 printf("size of an int:%d byte(s)\n", (int)sizeof(int));
 printf("size of a long int:%d byte(s)\n", (int)sizeof(long int));
 printf("size of a long long int:%d byte(s)\n", (int)sizeof(long long int));
