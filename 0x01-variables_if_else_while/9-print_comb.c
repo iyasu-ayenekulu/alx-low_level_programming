40092320aefb6a10d1c35ffe271c7d3010e2cdc3
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-09 05:14:23 -0700

    code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,14 @@ int main(void)
 {
 int i;
 for (i = 48; i < 58; i++)
+{
 putchar(i);
-for (i = 'a'; i <= 'f'; i++)
-putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
