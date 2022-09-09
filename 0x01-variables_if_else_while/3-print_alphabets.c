9997fe7e82658674d79f13cc913dd0f014e1d6c1
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-09 04:42:47 -0700

    print capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+# include <studio.h>
+
+/**
+ * main - enyery point
+ *
+ * return: 0
+ */
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+putchar (a);
+a++;
+}
+a = 'A';
+while (a <= 'Z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return(0);
+}
+
+
