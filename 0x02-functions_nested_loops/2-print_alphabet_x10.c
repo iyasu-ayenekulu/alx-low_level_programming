90c5fc78a6c2d4c5c3713ca5fbf6698a93b97334
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-13 13:59:10 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,12 @@
  *
  * Return: 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10 (void)
 {
 char c, i;
-for(i=0; i<=9; i++)
+for (i = 0; i <= 9; i++)
 {
-for(c='a'; c<='z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
