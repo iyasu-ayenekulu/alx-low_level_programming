8f32d76fc46374d29e5059f0f71114631c9e21a9
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-13 17:32:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * main - print putchar
- * Return:always 0
+ * Return: always 0
  */
-int main(void )
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -14,3 +14,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+return (0);
+}
+
