3082572cc9fbb06a39ea3b76691270e5c0ea3e5e
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-15 12:46:34 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,8 +3,11 @@
 
 /**                                                                   
 *main - header file for prtotypes
-*void prototypes (void)                                                *int prototypes (int)                                                  */                                                  
+*void prototypes (void)
+*int prototypes (int)                                                  
+*/
 
+int _putchar(char c);
 void print_numbers(void)                                               void print_most_numbers(void);                                         void more_numbers(void)                                                void print_line(int n);                      
 void print_diagonal(int n);          
 void print_square(int size)                                            void print_triangle(int size;                                          void print_number(int n);                                              int _putchar(char);                           
