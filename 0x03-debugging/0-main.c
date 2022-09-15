9b66a724330cbcbc1f2b0ffa4babfdb248dbaf8a
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-14 23:08:45 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
- * Retur: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Retur: 0
+*/
 int main(void)
 {
 int i;
