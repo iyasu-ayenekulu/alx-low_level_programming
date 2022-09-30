9148a6856d9111d8ba37ab3bb91d0d244690bf84
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-09-30 00:28:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,27 @@
+fndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
+#include <stdio.h>
+
+
+
+int _putchar(char c);
+
+int main(int argc, char *argv[])
+
+int main(int argc, __attribute__((unused)) char *argv[])
+
+int main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
+
+
+
+#endif
